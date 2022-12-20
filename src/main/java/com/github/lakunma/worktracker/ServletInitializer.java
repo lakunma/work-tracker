23192e8200ea7c94707d535ab401f35b8f312201
Mike <lakunma@users.noreply.github.com>
2022-12-20 17:30:39 +0300

    Initial commit

---
+++ src/main/java/com/github/lakunma/worktracker/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.github.lakunma.worktracker;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WorkTrackerApplication.class);
+	}
+
+}
---
+++ src/test/java/com/github/lakunma/worktracker/WorkTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lakunma.worktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
